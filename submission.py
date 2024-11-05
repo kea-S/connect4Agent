12bae00a00823141dcc48fd3f12c2e784edb5b12
kea-S <keaharvan1@gmail.com>
2024-11-05 14:50:03 +0800

    time limit not exceeded but can't beat base agent

--- submission.py
+++ submission.py
@@ -1,7 +1,4 @@
 from game_utils import initialize, step, get_valid_col_id, is_end, is_win, is_valid_col_id
-from simulator import GameController, HumanAgent
-from connect_four import ConnectFour
-from localBaby import LocalBabyAgent
 import numpy as np
 
 # Task 2.1: Defeat the Baby Agent
@@ -199,11 +196,3 @@ class AIAgent(object):
         move, _ = minimax(state, 3, float('-inf'), float('inf'), True, PLAYER_ID)
 
         return move
-
-
-agent1 = AIAgent(player_id=1)
-agent2 = LocalBabyAgent(player_id=2)
-
-board = ConnectFour()
-game = GameController(board=board, agents=[agent1, agent2])
-game.run()
